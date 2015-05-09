9638bcda624a940196f15d1eebee11e43c5dfc45
Elton <eltonv93@gmail.com>
2015-05-09 14:39:28 -0700

    Intial commit

---
+++ src/adapter/proxyAutomobile.java
@@ -0,0 +1,63 @@
+package adapter;
+import java.io.IOException;
+import exception.AutoException;
+import util.FileIO;
+import model.*;
+
+public abstract class proxyAutomobile {
+	private AutomobileCollection autoColl;
+	
+	public proxyAutomobile()
+	{
+		autoColl = new AutomobileCollection();
+	}
+	
+	public AutomobileCollection getAutoColl() {
+		return autoColl;
+	}
+
+	public void setAutoColl(AutomobileCollection autoColl) {
+		this.autoColl = autoColl;
+	}
+
+	public void updateOptionSetName(String modelName, String optionSetName,
+			String newName) 
+	{
+		autoColl.getAutoMap().get(modelName).setOptionsetName(autoColl.getAutoMap().get(modelName).findOpset(optionSetName), newName);
+	}
+
+	public void updateOptionPrice(String modelName, String optionSetName,
+			String optionName, float newPrice) 
+	{
+		autoColl.getAutoMap().get(modelName).setOptionPrice(autoColl.getAutoMap().get(modelName).findOpset(optionSetName), autoColl.getAutoMap().get(modelName).findOption(optionSetName, optionName), newPrice);
+	}
+
+	public void buildAuto(String fileName) throws AutoException 
+	{
+		Automobile a1 = null;
+		FileIO io = new FileIO();
+		do {
+			try {
+				a1 = io.readFile(fileName);
+			} catch (AutoException e) {
+				if( e.geterrorNo()==101)
+				{
+					fileName = e.fixFileNotRead();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} while(a1 == null);
+		autoColl.addAuto(a1.getName(), a1);
+	}
+
+	public void printAuto(String modelName) 
+	{
+		try {
+			autoColl.getAutoMap().get(modelName).printAll();
+		} catch (NullPointerException e) {
+			 new AutoException(106,"Model not found.");
+		}
+	}
+
+}
